51dbcb3a9f74a31008b1c27d2347e8169f30e250
Katherine Griffin <kag728@gmail.com>
2020-02-16 19:02:58 -0500

    Creates skeleton base app UI

--- ThriveApp/ThriveApp/ContentView.swift
+++ ThriveApp/ThriveApp/ContentView.swift
@@ -10,7 +10,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+        Text("Thrive")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .padding(5)
+        Text("Thriving on the Spectrum")
+            .font(.title)
+                .padding(5)
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+            Text("Sign In")
+            .foregroundColor(.blue)
+            .font(.title)
+            .padding()
+            .border(Color.blue, width: 5)
+            }.padding()
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+            Text("Sign Up")
+                .foregroundColor(.blue)
+                .font(.title)
+                .padding()
+                .border(Color.blue, width: 5)
+        }
+        }
     }
 }
 
