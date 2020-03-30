169c3f2495bcebe112e5d70aaa745c0fe9f83d9f
Katherine Griffin <kag728@gmail.com>
2020-03-30 08:31:56 -0400

    comm portal fixes posts

---
+++ ThriveApp/ThriveApp/Post.swift
@@ -0,0 +1,37 @@
+//
+//  Post.swift
+//  ThriveApp
+//
+//  Created by Katherine Griffin on 3/30/20.
+//  Copyright © 2020 Katherine Griffin. All rights reserved.
+//
+
+import SwiftUI
+import FirebaseAuth
+
+/// Post
+struct Post: Identifiable {
+    
+    /// unique id
+    var id: String = UUID().uuidString
+    
+    /// user
+    let user: String?
+    
+    /// post image
+    let image: String?
+    
+    /// post content
+    let content: String?
+    
+    /// post time
+    let time: String!
+    
+    /// Init
+    init(image: String?, content: String?, time: String) {
+        self.user = (Auth.auth().currentUser?.email)!
+        self.image = image
+        self.content = content
+        self.time = time
+    }
+}
